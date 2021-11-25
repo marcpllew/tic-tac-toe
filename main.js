80c9534584909635c9ffaa6ce86a3aae2e894944
marc <marcpllew@gmail.com>
2021-11-25 22:43:54 +1100

    Add in some CSS styling

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let turnCount = 0;
 let player1 = '0';
 let player2 = 'X';
+const statusDisplay = document.querySelector('.game--status');
+
 let board = [
-    // nested array to layout game board
+    // nested array to layout (virtual board) game board, every element in array is representative of a box
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
@@ -18,12 +20,13 @@ function clickedBoxes(event) {
     turnCount = turnCount + 1; // <= this means turnCout variable will incrememnt by 1 per click as this is inside the onClick function
 
     if (turnCount % 2 == 0) {
+        // if statement is determaning what character  each player is inputting into each box based on it being odd or even.
         // checking for even numbers
-        event.target.innerText = player2; // accesing the event property  and targeting the specific HTML Element player 2 has clicked
+        event.target.innerText = player2; // accesing the event property  and targeting the specific HTML Element player 2 has clicked, when box has been clicked
     } else {
         event.target.innerText = player1; //same as above for player 1
     }
-    let boxes = document.querySelectorAll('.box'); //returns all 'box' elements in HTML
+    let boxes = document.querySelectorAll('.box'); //returns all 'box'class elements in HTML
     // console.log(boxes);
 
     // console.log(board);
@@ -31,17 +34,16 @@ function clickedBoxes(event) {
         // looping through boxes
         // console.log(boxes[i].dataset.row);
         board[boxes[i].dataset.row][boxes[i].dataset.column] =
-            boxes[i].textContent; // Dont understand this bit? Is this populating all my data-row, data-column elemnts from HTML into the board array above
-        // will return all the contents of 'boxes'
+            boxes[i].textContent;
+        // will return all the contents of 'boxes'. dataset is accesing the content of row and column, and populating the board array
     }
-
+    console.log(board);
     if (
         board[1][1] !== '' &&
         board[0][0] === board[1][1] &&
         board[1][1] === board[2][2]
     ) {
         console.log('winner');
-
         alert('You Win!');
     } else if (
         board[0][1] !== '' &&
@@ -94,7 +96,7 @@ function clickedBoxes(event) {
         alert('You Win!');
     } else if (turnCount >= 9) {
         console.log('Game Over! Try Again');
-        alert('Game Over! Try Again!');
+        alert('Game Over! Try Again');
     }
 }
 //creat a function, loop through boxes reset turncount:
@@ -104,7 +106,6 @@ function restartGame() {
     //reset the textcontent on each box element in the DOM(page)
     turnCount = 0;
     board = [
-        // nested array to layout game board
         ['', '', ''],
         ['', '', ''],
         ['', '', ''],
